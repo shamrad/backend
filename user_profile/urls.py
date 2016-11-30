a37ff3024984b3b6e15b0186de8f82d032e6b819
zamani.shayan <zamani.shayan@yahoo.com>
2016-11-30 23:11:25 +0330

    linking anchors - modifying create writing page

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^register/', UserFormView.as_view(), name='register'),
     url(r'^login/', LoginView.as_view(), name='login'),
     url(r'^(?P<username>[a-zA-Z0-9_.]{4,})/$', index, name='index'),
-    url(r'^(?P<username>[a-zA-Z0-9_.]{4,})/(?P<pk>[0-9])/$', writing, name='writing'),
+    url(r'^(?P<username>[a-zA-Z0-9_.]{4,})/(?P<pk>[0-9]+)/$', writing, name='writing'),
     url(r'^(?P<username>[a-zA-Z0-9_.]{4,})/addnew/$', CreateWriting.as_view(), name='newriting'),
 ]
